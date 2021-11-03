61fd5ed23261a20bf334adcb568d890b30a5385c
Rubem Dornas <rapdornas@gmail.com>
2021-11-03 00:06:27 -0300

    Ajustes de front end

--- app/models/service.rb
+++ app/models/service.rb
@@ -9,6 +9,6 @@ class Service < ApplicationRecord
     "Churrasco" => "https://images.unsplash.com/photo-1560614382-33bd4daa1b9e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2370&q=80",
     "Compras" => "https://images.unsplash.com/photo-1581646741691-6ce169c27dec?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2375&q=80",
     "Limpeza de grelhas e espetos" => "https://wildwoodgrilling.s3.amazonaws.com/wp-content/uploads/2016/03/08214843/Cleaning-Grill-S.jpg",
-    "Toca pagode" => "https://images.unsplash.com/photo-1563980337510-bfdd2649a4bc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=987&q=80"
+    "Toca pagode" => "https://www.portalabrantes.com.br/images/noticias/16159/pandeiro_insta696x696.jpg"
   }
 end
