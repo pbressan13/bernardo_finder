a29347bceb1c461cf01e28c69f13f55b169fa892
Pedro Bressan <pbressan13@gmail.com>
2021-11-04 18:27:10 -0300

    Devise fields and facebook integration

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   resources :bookings
   resources :services
   post 'bookings/:id/rating', to: 'bookings#create_rating'
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' } do
+    delete 'sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
+  end
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
