7908d7cbf1f79d0f7fc53af5985baaec052384fc
Pedro Bressan <pbressan13@gmail.com>
2021-11-05 18:14:54 -0300

    Bookings

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-
-  resources :bookings
-  resources :services
+  resources :services do
+    resources :bookings
+  end
   post 'bookings/:id/rating', to: 'bookings#create_rating'
+  get 'users/:user_id/bookings', to: 'bookings#index', as: :my_bookings
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' } do
     delete 'sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
   end
