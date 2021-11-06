6e0f69315c517dff538252b08e43ad3347ab849b
Rubem Dornas <rapdornas@gmail.com>
2021-11-06 12:49:57 -0300

    Imagens no search

--- app/models/service.rb
+++ app/models/service.rb
@@ -8,6 +8,12 @@ class Service < ApplicationRecord
     message: "Serviço já cadastrado."
   }
 
+  validates :available, presence: true
+  validates :available_at, presence: true
+  validates :price_hour, presence: true
+  validates :min_duration, presence: true
+  validates :max_duration, presence: true
+
   IMAGES = {
     "Churrasco" => "https://images.unsplash.com/photo-1560614382-33bd4daa1b9e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2370&q=80",
     "Compras" => "https://images.unsplash.com/photo-1581646741691-6ce169c27dec?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2375&q=80",
